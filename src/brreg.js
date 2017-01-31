8ad33b9c13ee4ca1a8815a73d4c8ca12b3d86087
Harald Ekroth Svendberg <harald@svendberg.net>
2017-01-31 09:46:27 +0100

    Updated brreg bookmarklet

--- src/brreg.js
+++ src/brreg.js
@@ -1,4 +1,3 @@
-/*exported BrReg */
 var BrReg = (function () {
     var newDiv;
     var Xrm = frames[0].Xrm;
@@ -14,25 +13,31 @@ var BrReg = (function () {
         var field = document.getElementById('field');
         field.onkeyup = debounce(function () {
             var query = document.getElementById('field').value;
-            ajax.get(remote, { query: query }, function (result) {
-                var list = document.getElementById('results');
-                if (result && result.entries && result.entries.length > 0) {
-                    result.entries.forEach(function (item) {
-                        var newLi = document.createElement('li');
-                        newLi.id = item.orgnr;
-                        newLi.innerHTML = item.navn +
-                            "<span style='color: #444'> (" + item.orgnr + ")</span>" +
-                            "<br /><span>" +
-                            (item.forretningsadr != '' ? item.forretningsadr + "<br />" : '') +
-                            item.forradrpostnr + " " + item.forradrpoststed + "</span>";
-                        list.appendChild(newLi);
+            var list = document.getElementById('results');
+            while (list.firstChild) {
+                list.removeChild(list.firstChild);
+            }
+            if (query.length > 2) {
+                ajax.get(remote, { query: query }, function (result) {
+                    if (result && result.entries && result.entries.length > 0) {
+                        result.entries.forEach(function (item) {
+                            var newLi = document.createElement('li');
+                            newLi.setAttribute("style", styles.element);
+                            newLi.id = item.orgnr;
+                            newLi.innerHTML = "<b>" + item.navn + "</b>" + 
+                                "<span> (" + item.orgnr + ")</span>" +
+                                "<br /><span>" +
+                                (item.forretningsadr != '' ? item.forretningsadr + "<br />" : '') +
+                                item.forradrpostnr + " " + item.forradrpoststed + "</span>";
+                            list.appendChild(newLi);
 
-                        document.getElementById(item.orgnr).onclick = function() { 
-                            sendToCrm(item) 
-                        };
-                    });
-                };
-            });
+                            document.getElementById(item.orgnr).onclick = function () {
+                                sendToCrm(item)
+                            };
+                        });
+                    };
+                });
+            }
         }, 500);
     };
 
@@ -49,22 +54,41 @@ var BrReg = (function () {
         Xrm.Page.getAttribute("address1_city").setValue(item.forradrpoststed);
         Xrm.Page.getAttribute("address1_postalcode").setValue(item.forradrpostnr);
         Xrm.Page.getAttribute("accountnumber").setValue(item.orgnr);
+        Xrm.Page.getAttribute("websiteurl").setValue(item.url);
         hide();
     };
 
     var styles = function () {
         var obj = {};
+
+        obj.element = 'padding:6px 12px;' +
+            'border:1px solid transparent;' +
+            'border-radius:4px;' +
+            'border-color:#adadad;' +
+            'background-color:#ffffff;' +
+            'cursor:pointer;' +
+            'font-size:14px;' +
+            'color:#333333;' + 
+            'margin-bottom:5px;' 
+
         obj.box = 'position:absolute;' +
-            'top:30%;' +
+            'top:10%;' +
             'left:30%;' +
+            'width:500px;' +
             'box-shadow:0 5px 15px rgba(0, 0, 0, 0.5);' +
             'border:1px solid rgba(0, 0, 0, 0.2);' +
             'border-radius:6px;' +
-            'padding:30px;' +
+            'padding-top:30px;' +
+            'padding-left:30px;' +
+            'padding-right:30px;' +
+            'padding-bottom:0px;' +
             'font-family:Helvetica Neue, Helvetica, Arial, sans-serif;' +
             'color:#333333;' +
             'font-size:14px;' +
             'background-color:#ffffff;';
+        
+        obj.innerbox = 'overflow-y:auto;' + 
+            'max-height: 300px;';
 
         obj.input = 'padding: 6px 12px;' +
             'border: 1px solid #cccccc;' +
@@ -80,20 +104,23 @@ var BrReg = (function () {
             'cursor:pointer;' +
             'font-size:14px;' +
             'color:#333333;' +
-            'background-image:none;' +
-            'height:100%;';
+            'background-image:none;' + 
+            'height:40px;';
 
         obj.button_b = 'background-color:#428bca!important;' +
             'color:#ffffff!important;' +
             obj.button;
+
         return obj;
     } ();
 
     var markup = function () {
         var obj = {};
-        obj.box = '<div id="innerBox" style="' + styles.box + '"><strong>Firmanavn:</strong><br /><input type="text" id="field" style="' + styles.input + '"></select><br />' +
-            '<br /><br /><ul id="results"></ul><br />' +
-            '&nbsp;<button id="closeBtn" style="' + styles.button + '">Lukk</button></div>';
+        obj.box = '<div id="innerBox" style="' + styles.box + '" class="box"><strong>Søk etter firmanavn</strong><br />' + 
+            'Klikk på firma for å velge<br/><input type="text" id="field" style="' + styles.input + '"></select><br />' +
+            '<br /><br /><div style="' + styles.innerbox + '"><ul id="results"></ul></div><br />' +
+            '&nbsp;<button id="closeBtn" style="' + styles.button + '">Lukk</button><br/><br/>' + 
+            '<span><span style="float:left;line-height:50px;">Data hentes fra Brønnøysund</span><span style="float:right;line-height:50px;"><a href="codemover.com" target="_blank"><img src="https://codemover.azureedge.net/logo_xs.png"></img></a></span></span></div>';
         return obj;
     } ();
 
